ecc5cd990fc45c792a5868af1ec61cfba23b3025
Arpitapathak571 <151984552+Arpitapathak571@users.noreply.github.com>
2023-11-25 10:00:10 +0530

    Add files via upload

---
+++ c++ programing/first.c
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace.std:
+class person
+{
+    int age;
+    string name;
+    string address;
+    void input(){
+    cout<<"enter the age";
+    cin>>age;
+    cout<<"enter the name";
+    cin>>name;
+    cout<<"enter the address";
+    cin<<address;
+}
+void show()
+{
+    cout<<age;
+    cout<<name;
+    cout<<address;
+}
+};
+void main()
+{
+    person.ankit;
+    ankit.input();
+    ankit.show();
+}
+return 0;
+}
